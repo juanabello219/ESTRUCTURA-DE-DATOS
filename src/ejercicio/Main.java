6a50582ca788a570e4e7503fd36d2cc58548096a
JUAN ABELLO <128939966+juanabello219@users.noreply.github.com>
2026-02-24 07:35:48 -0500

    commit

---
+++ src/ejercicio/Main.java
@@ -0,0 +1,18 @@
+package ejercicio;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+     Scanner Entry = new Scanner(System.in);
+     System.out.println("SISTEMA DE CAJEROS Y SUPERMERCADOS");
+     precios = double [];
+
+     System.out.println("ingrese el valor de los 5 productos ingresados:");
+     int precio1 = Entry.nextInt();
+     int precio2 = Entry.nextInt();
+     int precio3 = Entry.nextInt();
+     int precio4 = Entry.nextInt();
+     int precio5 = Entry.nextInt();
+
+  }
+}
